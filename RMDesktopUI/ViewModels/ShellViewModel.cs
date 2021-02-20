9af9b3fba2e4d28469187d0f99e2c1d93719b118
wxx <wxx0136@gmail.com>
2021-02-20 16:01:57 -0600

    Temp Save

--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Caliburn.Micro;
 
 namespace RMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginViewModel;
+
+        public ShellViewModel(LoginViewModel loginViewModel)
+        {
+            _loginViewModel = loginViewModel;
+            ActivateItem(_loginViewModel);
+        }
+
     }
 }
