94acf70f544eb9a7d6ccb1381ba5879c0b823621
wxx <wxx0136@gmail.com>
2021-02-20 14:05:28 -0600

    Register DB Tables
    Use DBO to publish the tables to the db.

---
+++ RMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Id] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [FIrstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(256) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
